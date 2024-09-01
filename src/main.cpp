c8d71fec7fbad23b9a75bccc2423333377502087
Arun <amuthu165@gmail.com>
2024-09-01 12:46:45 +0530

    imageNotRendered

--- src/main.cpp
+++ src/main.cpp
@@ -15,5 +15,5 @@ crow::mustache::rendered_template render_page(){
         crow::mustache::set_base("./src/templates");
         // Load the template from a file (or define it directly as a string)
         auto page = crow::mustache::load("./index.html").render();
-        return page;  // Send the rendered HTML page
+        return page;  // Send the rendered HTML page.
 }
