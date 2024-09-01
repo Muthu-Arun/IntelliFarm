d14cea73f5e899c35e45e109cf26b17336d3b28c
Arun <amuthu165@gmail.com>
2024-09-01 17:15:42 +0530

    latest working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<crow_all.h>
+#include<fstream>
 crow::mustache::rendered_template render_page();
 int main()
 {
@@ -8,6 +9,29 @@ int main()
     CROW_ROUTE(app, "/")(render_page);
     
     //set the port, set the app to run on multiple threads, and run the app
+      app.route_dynamic("/static/<string>")
+        .methods("GET"_method)
+        ([](const crow::request&, crow::response& res, std::string filename){
+            std::string file_path = "./static/css/styles.css"; // Adjust the path as needed
+            std::ifstream in(file_path, std::ifstream::in);
+
+            if (!in) {
+                res.code = 404;
+                res.end("File not found");
+                return;
+            }
+
+            std::ostringstream contents;
+            contents << in.rdbuf();
+            res.write(contents.str());
+        
+                res.add_header("Content-Type", "text/css");
+
+            // Add more content types if necessary
+
+            res.end();
+        });
+
     app.port(18080).multithreaded().run();
 }
   
