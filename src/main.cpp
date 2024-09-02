3a6fb2eb0843371bd054fc7787bc55e9902e00cf
Arun <amuthu165@gmail.com>
2024-09-02 18:54:55 +0530

    Squashed commit of the following:
    commit 2ff3af36fa2ff1cf7d3b3f846c92b7f151c0677f
    Author: Arun <amuthu165@gmail.com>
    Date:   Mon Sep 2 17:48:24 2024 +0530
    
        working commit with cache
    
    commit d619ecf70b602e52c3f881bd36f0c6e51584ccae
    Author: Arun <amuthu165@gmail.com>
    Date:   Sun Sep 1 21:06:04 2024 +0530
    
        last not working - image
    
    commit 48390d852a6feb94f5ed9082581c9ec1f42ebb20
    Author: Arun <amuthu165@gmail.com>
    Date:   Sun Sep 1 20:57:14 2024 +0530
    
        css image 3 fix
    
    commit eddf0e6ed76c83faa5ab7d9f74694d342584ede8
    Merge: 6c9cda0 ebd1763
    Author: Prajit <prajit016@gmail.com>
    Date:   Sun Sep 1 20:19:45 2024 +0530
    
        Merge branch 'dev' of https://github.com/Muthu-Arun/esc into dev
    
    commit 6c9cda0c49ebe19e4661cedb6a2620fae6aa0d5d
    Author: Prajit <prajit016@gmail.com>
    Date:   Sun Sep 1 20:19:02 2024 +0530
    
        changes of image
    
    commit a074bac29ad9b02a9a022cfd2f0bee6b3ed12303
    Author: Prajit <prajit016@gmail.com>
    Date:   Sun Sep 1 20:18:34 2024 +0530
    
        first stage image
    
    commit ebd1763e621e1a3df165448b0af596bfa4349525
    Author: Arun <amuthu165@gmail.com>
    Date:   Sun Sep 1 20:16:37 2024 +0530
    
        Final Wroking Start day
    
    commit 8d64fe94921cf2346c7f936446d6138a42b617b3
    Author: Prajit <prajit016@gmail.com>
    Date:   Sun Sep 1 17:37:46 2024 +0530
    
        Changes of HTML and CSS
    
    commit 779757cac777df6c06f092a2be23b927aa17c27d
    Author: Prajit <prajit016@gmail.com>
    Date:   Sun Sep 1 17:23:43 2024 +0530
    
        first stage css
    
    commit d14cea73f5e899c35e45e109cf26b17336d3b28c
    Author: Arun <amuthu165@gmail.com>
    Date:   Sun Sep 1 17:15:42 2024 +0530
    
        latest working
    
    commit 25eb1ac7af06ef7a7660d1f0170520ae81006f72
    Author: Prajit <prajit016@gmail.com>
    Date:   Sun Sep 1 16:54:49 2024 +0530
    
        first prajit upload
    
    commit aa7a4ec9619d3230da9236221a07720e7a94c09c
    Author: Prajit <prajit016@gmail.com>
    Date:   Sun Sep 1 16:53:08 2024 +0530
    
        first html  commit
    
    commit d16cb56b9c92261fee516c8be4991041b1b1b516
    Author: Prajit <prajit016@gmail.com>
    Date:   Sun Sep 1 16:50:33 2024 +0530
    
        summa

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,58 @@
 #include<iostream>
 #include<crow_all.h>
+#include<fstream>
+// #define CROW_STATIC_DIRECTORY "./src/static" 
 crow::mustache::rendered_template render_page();
 int main()
 {
     crow::SimpleApp app; //define your crow application
     //define your endpoint at the root directory
     CROW_ROUTE(app, "/")(render_page);
-    
-    //set the port, set the app to run on multiple threads, and run the app
-    app.port(18080).multithreaded().run();
-}
+    CROW_ROUTE(app, "/static/css/styles.css")([](){
+        std::ifstream file("./src/static/css/styles.css",std::ios::binary);
+        if (!file.is_open()) {
+            std::cout<<"FIle not opened\n";
+            return crow::response(404);
+            
+        }
 
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        crow::response res(buffer.str());
+
+        // Set the appropriate content type based on the file extension
+
+        //  if (filename.find(".css") != std::string::npos) {
+            res.set_header("Content-Type", "text/css");
+        //  }
+
+        return res;
+    });
+        CROW_ROUTE(app, "/static/images/image3.jpg")([](){
+        std::ifstream file("./src/static/images/image3.jpg",std::ios::binary);
+        if (!file.is_open()) {
+            std::cout<<"FIle not opened\n";
+            return crow::response(404);
+            
+        }
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        crow::response res(buffer.str());
+        res.set_header("Cache-Control", "public, max-age=86400"); // Cache for 1 day
+
+        // Set the appropriate content type based on the file extension
+
+        //  if (filename.find(".css") != std::string::npos) {
+        res.set_header("Content-Type", "image/jpeg");
+
+        //  }
+
+        return res;
+    });
+    app.bindaddr("172.18.162.174").port(8080).multithreaded().run();
+}
+  
 crow::mustache::rendered_template render_page(){
         crow::mustache::set_base("./src/templates");
         // Load the template from a file (or define it directly as a string)
